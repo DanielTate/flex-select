c03ee64d204f0fd898fa29e75cacf67a8f763ca3
Dan <dan@tate.nz>
2021-03-23 16:14:07 +1300

    Update build process

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,19 +1,56 @@
-const { build } = require('esbuild')
-const postCssPlugin = require('@deanc/esbuild-plugin-postcss')
+const { startService } = require('esbuild')
+const { watch } = require('chokidar')
 
-build({
-    entryPoints: ['./src/flex-select.js', './src/flex-select.css'],
-    outdir: './build',
-    minify: true,
-    bundle: true,
-    sourcemap: true,
-    plugins: [
-        postCssPlugin()
-    ],
-    watch: {
-        onRebuild(error, result) {
-            if (error) console.error('watch build failed: ', error)
-            else console.error('watch build succeeded: ', result)
-        }
+const noop = () => {};
+
+/**
+ * Function to update lines when something happens
+ * @param input The text you want to print
+ * @param isBuiltInput Whether you are printing `Built in x ms` or not
+ */
+const updateLine = (input, isBuiltInput = false) => {
+    const numberOfLines = (input.match(/\n/g) || []).length;
+    process.stdout.cursorTo(0, 2);
+    process.stdout.clearScreenDown();
+    process.stdout.write(input);
+    isBuiltInput ? process.stdout.moveCursor(0, -numberOfLines) : noop();
+};
+
+/**
+ * Builds the code in no time
+ */
+const build = async () => {
+    //Start build service
+    const service = await startService();
+    try {
+        process.stdout.cursorTo(0, 2);
+        process.stdout.clearLine(0);
+        // Get time before build starts
+        const timerStart = Date.now();
+        // Build code
+        await service.build({
+            color: true,
+            entryPoints: ["./src/flex-select.js", "./src/flex-select.css"],
+            outdir: "./build",
+            minify: true,
+            bundle: true,
+            sourcemap: false,
+            logLevel: "error",
+        });
+        // Get time after build ends
+        const timerEnd = Date.now();
+        updateLine(`Built in ${timerEnd - timerStart}ms.`, true);
+    } catch (e) {
+        // OOPS! ERROR!
+    } finally {
+        // We command you to stop. Will start again if files change.
+        service.stop();
     }
-}).catch(() => process.exit(1))
+};
+
+const watcher = watch(["src/**/*"]);
+console.log("Watching files... \n");
+build();
+watcher.on("change", () => {
+    build();
+});
